dc91a18df4630a32820553e5f268f3c86d942780
vishakha011 <khatadevishakha01@gmail.com>
2021-05-18 07:14:17 +0530

    Added instance variables in Tasks controller

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import { initializeLogger } from "./common/logger";
+import { initializeLogger } from "common/logger";
 const App = () => {
   useEffect(() => {
     /*eslint no-undef: "off"*/
